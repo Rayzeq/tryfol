76378c568bc1d6590ec00351c1bae7e3b07d6f75
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2025-01-29 18:43:22 +0100

    feat(tryfol-bar/power): add `darkman` module

---
+++ tryfol-bar/src/modules/power/darkman.rs
@@ -0,0 +1,25 @@
+use std::process::Stdio;
+
+use gtk::{glib, prelude::*, Button};
+use gtk4 as gtk;
+use log::error;
+use tokio::process::Command;
+
+pub fn new() -> Button {
+    let button = Button::builder().name("darkman").label("").build();
+    button.connect_clicked(|_| {
+        match Command::new("darkman")
+            .arg("toggle")
+            .stdout(Stdio::null())
+            .spawn()
+        {
+            Ok(mut child) => {
+                // Prevent zombie process
+                glib::spawn_future_local(async move { child.wait().await });
+            }
+            Err(e) => error!("Failed to run darkman: {}", e),
+        }
+    });
+
+    button
+}
--- tryfol-bar/src/modules/power/mod.rs
+++ tryfol-bar/src/modules/power/mod.rs
@@ -1,18 +1,22 @@
-mod battery;
-mod brigthness;
-
 use gtk4::{self as gtk, prelude::*};
 
+mod battery;
+mod brigthness;
+mod darkman;
+
 pub fn new() -> gtk::Box {
-    let brightness = brigthness::new();
-    brightness.add_css_class("right");
+    let darkman = darkman::new();
+    let battery = battery::new();
+    battery.add_css_class("left");
+    darkman.add_css_class("right");
 
     let root = gtk::Box::builder()
         .name("battery")
         .css_classes(["module"])
         .build();
-    root.append(&battery::new());
-    root.append(&brightness);
+    root.append(&battery);
+    root.append(&brigthness::new());
+    root.append(&darkman);
 
     root
 }
